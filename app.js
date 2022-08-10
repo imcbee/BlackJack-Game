023dc292cbecfcc93fcbd57b1ae1e8d7c760457b
Ian McBee <ianmcbee@Ians-MacBook-Air.local>
2022-08-09 21:53:36 -0400

    updates 09Aug22

--- app.js
+++ app.js
@@ -1,81 +1,11 @@
 
 
-
-
-
-
-// class PlayingCards {
-//     constructor(number, symbol, color) {
-//         this.number = number;
-//         this.symbol = symbol;
-//         this.color = color;
-//     }
-//     //try to have a method function for Aces?
-// };
-// const playingCards = [
-//  {twoDiamond: new PlayingCards(2, "Diamond", "Red")},
-//  {twoHeart: new PlayingCards(2, "Heart", "Red")},
-//  {twoClub: new PlayingCards(2, "Club", "Black")},
-//  {twoSpade: new PlayingCards(2, "Spade", "Black")},
-//  {threeDiamond: new PlayingCards(3, "Diamond", "Red")},
-//  {threeHeart: new PlayingCards(3, "Heart", "Red")},
-//  {threeClub: new PlayingCards(3, "Club", "Black")},
-//  {threeSpade: new PlayingCards(3, "Spade", "Black")},
-//  {fourDiamond: new PlayingCards(4, "Diamond", "Red")},
-//  {fourHeart: new PlayingCards(4, "Heart", "Red")},
-//  {fourClub: new PlayingCards(4, "Club", "Black")},
-//  {fourSpade: new PlayingCards(4, "Spade", "Black")},
-//  {fiveDiamond: new PlayingCards(5, "Diamond", "Red")},
-//  {fiveHeart: new PlayingCards(5, "Heart", "Red")},
-//  {fiveClub: new PlayingCards(5, "Club", "Black")},
-//  {fiveSpade: new PlayingCards(5, "Spade", "Black")},
-//  {sixDiamond: new PlayingCards(6, "Diamond", "Red")},
-//  {sixHeart: new PlayingCards(6, "Heart", "Red")},
-//  {sixClub: new PlayingCards(6, "Club", "Black")},
-//  {sixSpade: new PlayingCards(6, "Spade", "Black")},
-//  {sevenDiamond: new PlayingCards(7, "Diamond", "Red")},
-//  {sevenHeart: new PlayingCards(7, "Heart", "Red")},
-//  {sevenClub: new PlayingCards(7, "Club", "Black")},
-//  {sevenSpade: new PlayingCards(7, "Spade", "Black")},
-//  {eightDiamond: new PlayingCards(8, "Diamond", "Red")},
-//  {eightHeart: new PlayingCards(8, "Heart", "Red")},
-//  {eightClub: new PlayingCards(8, "Club", "Black")},
-//  {eightSpade: new PlayingCards(8, "Spade", "Black")},
-//  {nineDiamond: new PlayingCards(9, "Diamond", "Red")},
-//  {nineHeart: new PlayingCards(9, "Heart", "Red")},
-//  {nineClub: new PlayingCards(9, "Club", "Black")},
-//  {nineSpade: new PlayingCards(9, "Spade", "Black")},
-//  {tenDiamond: new PlayingCards(10, "Diamond", "Red")},
-//  {tenHeart: new PlayingCards(10, "Heart", "Red")},
-//  {tenClub: new PlayingCards(10, "Club", "Black")},
-//  {tenSpade: new PlayingCards(10, "Spade", "Black")},
-//  {jackDiamond: new PlayingCards(10, "Diamond", "Red")},
-//  {jackHeart: new PlayingCards(10, "Heart", "Red")},
-//  {jackClub: new PlayingCards(10, "Club", "Black")},
-//  {jackSpade: new PlayingCards(10, "Spade", "Black")},
-//  {queenDiamond: new PlayingCards(10, "Diamond", "Red")},
-//  {queenHeart: new PlayingCards(10, "Heart", "Red")},
-//  {queenClub: new PlayingCards(10, "Club", "Black")},
-//  {queenSpade: new PlayingCards(10, "Spade", "Black")},
-//  {kingDiamond: new PlayingCards(10, "Diamond", "Red")},
-//  {kingHeart: new PlayingCards(10, "Heart", "Red")},
-//  {kingClub: new PlayingCards(10, "Club", "Black")},
-//  {kingSpade: new PlayingCards(10, "Spade", "Black")},
-//  {aceDiamond: new PlayingCards(11, "Diamond", "Red")},
-//  {aceHeart: new PlayingCards(11, "Heart", "Red")},
-//  {aceClub: new PlayingCards(11, "Club", "Black")},
-//  {aceSpade: new PlayingCards(11, "Spade", "Black")},
-// ];
-// console.log(playingCards[9].fourHeart.number)
-
 let playerCards = [],dealerCards = [],clicked= false, currentIndex = 0;
 let playerSum =0, dealerSum =0;
-let playerMoney = 0,AIMoney1 = 0,AIMoney2 = 0;
+let playerMoney = 0;
+let discardPile = [];
 
-let deck = buildDeck();
-//console.log(deck)
-shuffleDeck(deck);
-//console.log(deck)
+
 
 
 
@@ -85,6 +15,7 @@ const reset = document.querySelector('#end-game');
 const bet = document.querySelector('#bet');
 const hit = document.querySelector('#hit');
 const stand = document.querySelector('#stand');
+const nextRound = document.querySelector('#round')
 const userCard5 = document.querySelector('#card5');
 const userCard6 = document.querySelector('#card6');
 const userCard7 = document.querySelector('#card7');
@@ -116,46 +47,45 @@ const messageBoard = document.querySelector('#message-board-container');
     function shuffleDeck(arr) {
         arr.sort(() => 0.5 - Math.random());
         
-        //return arr
+        //ret;urn arr
     }
+
+    
 
     //deal cards
     function dealCards(arr) {
-        playerCards.push(arr[0])
-        currentIndex++
-        dealerCards.push(arr[1])
-        currentIndex++
-        playerCards.push(arr[2])
-        currentIndex++
-        dealerCards.push(arr[3])
-        currentIndex++   
-    }
+        playerCards.push((arr.shift()));
+        currentIndex++;
+        dealerCards.push((arr.shift()));
+        currentIndex++;
+        playerCards.push((arr.shift()));
+        currentIndex++;
+        dealerCards.push((arr.shift()));
+        currentIndex++;  
+    };
 
     //hit cards for player
     function hitCards(arr) {
-        playerCards.push(arr[4])
+        playerCards.push((arr.shift()))
         currentIndex++
         
-    }
+    };
 
+    
+   
     
 
     //deal to dealer
     function dealToDealer(arr) {  //could be area of trouble, currentindex for later figuringout
         
-        if(dealerSum < 17) {
-            dealerCards.push(arr[5])
+        if(dealerSum <= 17) {
+            dealerCards.push((arr.shift()))
             //console.log(arr[5])
             currentIndex++
         }if(dealerSum > 17) {
-            return;
+            
         }
-        //wait()
-    }
-
-    //delay timer
-    function wait() {
-        return setTimeout(function(){},5000)
+        
     }
 
     //get value of card
@@ -202,39 +132,48 @@ const messageBoard = document.querySelector('#message-board-container');
             messageBoard.innerHTML = `Dealer Wins! Dealer Total: ${dealerSum}`
         }else if(playerSum > dealerSum && playerSum < 21) {
             messageBoard.innerHTML = `Player Wins! Player Total: ${playerSum}`
-        }else if (dealerSum === playerSum) [
+        }else if (dealerSum === playerSum) {
             messageBoard.innerHTML = `Draw! Dealer Total: ${playerSum}, Player Total: ${playerSum} `
-        ]
+        }
     }
 
 
 //EventListeners
-startGame.addEventListener('click', function() {
-    
-    bet.addEventListener('click', function() {
+
+
+
+bet.addEventListener('click', function() {
         console.log('what')
     });
 
+
+
+startGame.addEventListener('click', function() {
+    
+    
     hit.addEventListener('click', function() {
-        
-        hitCards(deck)
-        
+        hitCards(deck);
+        dealerMove();
         userCard7.innerHTML = playerCards[2];
         playerSum = cardValue(playerCards)  //cardvalue expects an array of strings, or the updated array
-        messageBoard.innerHTML = `Player Total: ${playerSum}`
-       
-        checkWin() 
-        console.log(`Dealer: ${dealerSum}`)
-        console.log(`player: ${playerSum}`)
+        messageBoard.innerHTML = `Player Total: ${playerSum}`;
+        checkWin(); 
+
+
+        // console.log(`Dealer: ${dealerSum}`)
+        // console.log(`player: ${playerSum}`)
         //console.log(playerCards)
         //console.log(currentIndex)
     });
 
     stand.addEventListener('click', function() {
-        //console.log('mhmm')
-        console.log(`Dealer: ${dealerSum}`)
-        console.log(`player: ${playerSum}`)
-        checkWin()
+        dealerMove();
+        checkWin();
+        
+        
+        
+        // console.log(`Dealer: ${dealerSum}`)
+        // console.log(`player: ${playerSum}`)
     })
     //playerCards.push(deck[0])
     
@@ -243,8 +182,65 @@ startGame.addEventListener('click', function() {
         if(clicked){window.location.reload();}
         clicked = false;
     });
+
+    nextRound.addEventListener('click', function() {
+        //discardPile = playerCards.concat(dealerCards);
+        for(let i =0;i<playerCards.length;i++) {
+            discardPile.push(playerCards[i])
+        };
+        for(let i =0;i<dealerCards.length;i++) {
+            discardPile.push(dealerCards[i])
+        };
+
+        playerCards = []; 
+        dealerCards = [];
+        dealerCard1.innerHTML = "";
+        dealerCard2.innerHTML = "";
+        dealerCard3.innerHTML = "";
+        dealerCard4.innerHTML = "";
+        userCard5.innerHTML = "";
+        userCard6.innerHTML = "";
+        userCard7.innerHTML = "";
+        userCard8.innerHTML = "";
+        dealCards(deck);
+        
+        dealerCard1.innerText = dealerCards[0]
+        dealerCard2.innerText = dealerCards[1]
+        userCard5.innerText = playerCards[0]
+        userCard6.innerText = playerCards[1]
+
+        checkWin();
+        discardShuffle(discardPile);
+        //console.log(playerCards, dealerCards);
+        // console.log(deck)
+        // console.log(discardPile)
+    })
     
+    //dealer move
+    function dealerMove() {
+        dealToDealer(deck)
+        dealerCard3.innerHTML = dealerCards[2];
+        dealerSum = cardValue(dealerCards)
+    };
+
+    //discard shuffle
+    function discardShuffle(arr) {
+        shuffleDeck(arr);
+        
+        while(arr.length > 25) {
+            deck.push(arr[arr.length-1]);
+            arr.pop()
+        };
+       
+        console.log(deck)
+        console.log(discardPile)
+    };
+   
     clicked = true;
+    let deck = buildDeck();
+    //console.log(deck)
+    shuffleDeck(deck);
+    //console.log(deck)
     dealCards(deck)
     
     dealerCard1.innerHTML = dealerCards[0]
@@ -264,16 +260,13 @@ startGame.addEventListener('click', function() {
     }if(playerSum === 21) {
         checkWin();
         clicked = false;
-    }
+    };
     
     
-    dealToDealer(deck)
-    dealerCard3.innerHTML = dealerCards[2];
-    dealerSum = cardValue(dealerCards)
-    //console.log(dealerCards)
     
-    console.log(`Dealer: ${dealerSum}`)
-    console.log(`player: ${playerSum}`)
+    
+    // console.log(`Dealer: ${dealerSum}`)
+    // console.log(`player: ${playerSum}`)
     //console.log(currentIndex)
     //console.log(dealerCards)
     
